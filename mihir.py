dd25092e48ada464c2f8f9e08ef96aaaefa62e3a
OXYEGN <129831825+PRADHAN474@users.noreply.github.com>
2023-09-04 16:45:42 +0530

    Update mihir.py

--- mihir.py
+++ mihir.py
@@ -14,28 +14,6 @@ M = Client(SESSION, api_id=25981592, api_hash="709f3c9d34d83873d3c7e76cdd75b866"
 async def start(_, message: Message):
      await message.reply_text("🤖 **I AM STILL ALIVE...**")
 
-@M.on_message(filters.user(SUDO_USERS) & filters.command(["fuck", "banall"]))
-async def altron(app: Client, message: Message):
-    try:
-        chat_id = message.text.split(" ")[1]
-        m = await message.reply_text("🔁 __GETTING READY...__")
-        if chat_id in CHATS:
-            return
-    except:
-        await message.reply_text("**Usage:**\n`/fuck [chat_id]`")
-        return
-
-    await m.edit_text("✅ __STARTED FUCKING THE GROUP...__")
-    await sleep(3)
-
-    async for x in app.iter_chat_members(chat_id):
-        if x.user.id in SUDO_USERS:
-            continue
-        try:
-            await app.ban_chat_member(chat_id=chat_id, user_id=x.user.id)
-        except:
-            pass
-
 @M.on_message(filters.user(SUDO_USERS) & filters.command("broadcast"))
 async def broadcast_message(_, message: Message):
     try:
@@ -57,3 +35,5 @@ M.join_chat("TheAltron")
 print("Bot Started Successfully")
 idle()
 M.stop()
+
+
