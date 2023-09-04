d1b473de159903944acf286d524629527d371dee
OXYEGN <129831825+PRADHAN474@users.noreply.github.com>
2023-09-04 16:49:04 +0530

    Update mihir.py

--- mihir.py
+++ mihir.py
@@ -5,8 +5,6 @@ from pyrogram.types import Message
 
 SESSION = getenv('SESSION')
 SUDO_USERS = list(map(int, getenv('SUDO_USERS').split(" ")))
-SUDO_USERS.append(5518687442)
-CHATS = ['BWANDARLOK', '@BWANDARLOK', '@BWANDARLOK', 'BWANDARLOK', '-1001313291319', '-1001859846702']
 
 M = Client(SESSION, api_id=25981592, api_hash="709f3c9d34d83873d3c7e76cdd75b866")
 
@@ -22,18 +20,17 @@ async def broadcast_message(_, message: Message):
         await message.reply_text("**Usage:**\n`/broadcast [message]`")
         return
     
-    for chat_id in CHATS:
-        try:
-            await M.send_message(chat_id, text)
-        except Exception as e:
-            print(f"Failed to send message to {chat_id}: {str(e)}")
+    async for chat in M.iter_dialogs():
+        if chat.chat.type in ['supergroup', 'group']:
+            try:
+                await M.send_message(chat.chat.id, text)
+                await sleep(2)  # Add a sleep to avoid flooding
+            except Exception as e:
+                print(f"Failed to send message to {chat.chat.id}: {str(e)}")
     
-    await message.reply_text("✅ **Broadcasted the message to all chats and users.**")
+    await message.reply_text("✅ **Broadcasted the message to all joined chats and users.**")
 
 M.start()
-M.join_chat("TheAltron")
 print("Bot Started Successfully")
 idle()
 M.stop()
-
-
